2cafbaebeaa77f01b6985f55390efa048e10f1fd
SimeonIloski <simeoniloski@gmail.com>
2019-07-16 15:12:48 +0200

    implement custom object,threadlocal,syhcnorizedblock,primitivetype

--- src/NewThread.java
+++ src/NewThread.java
@@ -6,7 +6,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class NewThread extends Thread {
     // Atomic integer containing the next thread ID to be assigned
     private static final AtomicInteger nextId   = new AtomicInteger(0);
-
+    static int n;
+    static Person person;
     // Thread local variable containing each thread's ID
     private static final ThreadLocal<Integer> threadId = new ThreadLocal<Integer>()
     {
@@ -38,19 +39,26 @@ public class NewThread extends Thread {
     {
         //printing the date of creating of the thread
         System.out.println("Starting Thread: \t : \t"+ getThreadId()+startDate.get());
-       try
-        {
-            TimeUnit.SECONDS.sleep((int) Math.rint(Math.random() * 10));
-            //getting random value for the thread squared and multiplied with the thread id
-            Random random=new Random();
-            int n=random.nextInt(50);
-            n=(n*n)*threadId.get();
-            System.out.println("Thread id is \t"+threadId.get());
-            System.out.println("The random generated number squared and multiplied with current threadID is \t"+n);
-
-        }  catch (InterruptedException e)
-        {
-            e.printStackTrace();
+       // using a ThreadLocal variable,primitive type variable and custom object
+        synchronized (this) {
+            try {
+                TimeUnit.SECONDS.sleep((int) Math.rint(Math.random() * 10));
+                //getting random value for the thread squared and multiplied with the thread id
+                Random random = new Random();
+                n = random.nextInt(50);
+                n = (n * n) * threadId.get();
+                //printing a Thread Local in synchronized block
+                System.out.println("Thread id is (printed in synchronized block like a thread local) \t" + threadId.get());
+                String name="personname"+ threadId.get();
+                String surname="personsurname"+threadId.get();
+                person=new Person(name,surname);
+                //printing a primitive number in  synchronized block
+                System.out.println("The random primitive generated number(in synchronized block) squared and multiplied with current threadID is \t" + n);
+                //printing a custom object in  synchronized block
+                System.out.println("My custom created object Person in synchronized block has name \t"+person.getName()+"\t and surname \t"+ person.getSurname());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("Thread Finished: \t : \t\n"+getThreadId()+startDate.get());
     }
